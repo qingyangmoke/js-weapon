126b653a769faa958f6afb4fca1c0a39fb5d1d51
采东 <fusheng.sfs@antfin.com>
2017-12-15 17:29:28 +0800

    修改编译顺序

--- script/build.js
+++ script/build.js
@@ -33,7 +33,7 @@ const building = ora('building...\n\n');
 building.start();
 
 new Promise((resolve, reject) => {
-  console.log(chalk.cyan(`1.clear dist directory ...\n\n`));
+  console.log(chalk.cyan(`## clear dist directory ...\n\n`));
   rm(path.resolve(buildPath, `*`), err => {
     if (err) {
       reject(err);
@@ -42,100 +42,106 @@ new Promise((resolve, reject) => {
     resolve();
   });
   console.log(chalk.green(`  clear complete\n\n`));
-}).then(() => {
-  return new Promise((resolve, reject) => {
-    console.log(chalk.cyan('2.copy LICENSE\n'));
-    copy(`${rootPath}/LICENSE`, distPath, function (err, files) {
-      if (err) {
-        reject(err);
-        return;
-      };
-      resolve();
-      console.log(chalk.green('  copying LICENSE complete.\n'));
+})
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      console.log(chalk.cyan('## webpack building... \n'));
+      webpack(config, function (err, stats) {
+        if (err) {
+          reject(err);
+          return;
+        };
+        building.stop();
+        process.stdout.write(stats.toString({
+          colors: true,
+          modules: false,
+          children: false,
+          chunks: false,
+          chunkModules: false,
+        }) + '\n\n');
+        console.log(chalk.green('  webpack Build complete.\n'));
+        resolve();
+      });
     });
-  });
-}).then(() => {
-  return new Promise((resolve, reject) => {
-    console.log(chalk.cyan('3.copy js files to dist ...\n\n'));
-    const folderList = fs.readdirSync(srcPath);
-    folderList.forEach((item, index) => {
-      var stat = fs.statSync(path.join(srcPath, item));
-      if (stat.isDirectory()) {
-        console.log(chalk.cyan(`  copying directory ${item}...\n`));
-        const files = fs.readdirSync(path.resolve(srcPath, item));
-        if (files.length > 0) {
-          files.forEach((file, index) => {
-            const srcFile = path.resolve(srcPath, item, file);
-            const distFile = path.resolve(distPath, file);
-            let data = fs.readFileSync(srcFile, { encoding: 'utf8' });
-            const matchData = data.match(/require\([\'\"]?[^\)]+\)/);
-            matchData && matchData.forEach((item) => {
-              if (item.indexOf('/') > -1) {
-                const newitem = item.substring(0, item.indexOf("'") + 1) + './' + item.substring(item.lastIndexOf("/") + 1);
-                data = data.replace(item, newitem);
-              }
+  })
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      console.log(chalk.cyan('## copy LICENSE\n'));
+      copy(`${rootPath}/LICENSE`, distPath, function (err, files) {
+        if (err) {
+          reject(err);
+          return;
+        };
+        resolve();
+        console.log(chalk.green('  copying LICENSE complete.\n'));
+      });
+    });
+  })
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      console.log(chalk.cyan('## copy js files to dist ...\n\n'));
+      const folderList = fs.readdirSync(srcPath);
+      folderList.forEach((item, index) => {
+        var stat = fs.statSync(path.join(srcPath, item));
+        if (stat.isDirectory()) {
+          console.log(chalk.cyan(`  copying directory ${item}...\n`));
+          const files = fs.readdirSync(path.resolve(srcPath, item));
+          if (files.length > 0) {
+            files.forEach((file, index) => {
+              const srcFile = path.resolve(srcPath, item, file);
+              const distFile = path.resolve(distPath, file);
+              let data = fs.readFileSync(srcFile, { encoding: 'utf8' });
+              const matchData = data.match(/require\([\'\"]?[^\)]+\)/);
+              matchData && matchData.forEach((item) => {
+                if (item.indexOf('/') > -1) {
+                  const newitem = item.substring(0, item.indexOf("'") + 1) + './' + item.substring(item.lastIndexOf("/") + 1);
+                  data = data.replace(item, newitem);
+                }
+              });
+              fs.writeFileSync(distFile, data, { encoding: 'utf8' });
+              console.log(chalk.green(`  copy file ${srcFile} ==> ${distFile} \n`));
             });
-            fs.writeFileSync(distFile, data, { encoding: 'utf8' });
-            console.log(chalk.green(`  copy file ${srcFile} ==> ${distFile} \n`));
-          });
+          }
         }
-      }
-      // copy(`src/${item}/*.js`, distPath, function (err, files) {
-      //   if (err) {
-      //     reject(err);
-      //     return;
-      //   };
-      //   console.log(chalk.green(`  copying ${item} complete.\n`));
-      //   if (index === folderList.length - 1) {
-      //     console.log(chalk.green('  Copy complete.\n'));
-      //     copying.stop();
-      //     resolve();
-      //   }
-      // });
-      // function replacePromise(filePath) {
-      //   return new Promise((resolve, reject) => {
-      //     const data = fs.readFileSync(filePath, { encoding: 'utf8' });
-      //     const result = data.replace(your - regex - or - text, replacement - text);
-      //     fs.writeFileSync(filePath, result, { encoding: 'utf8' });
-      //     resolve();
-      //   });
-      // }
-    });
-    resolve();
-  });
-}).then(() => {
-  return new Promise((resolve, reject) => {
-    console.log(chalk.cyan('4.webpack building... \n'));
-    webpack(config, function (err, stats) {
-      if (err) {
-        reject(err);
-        return;
-      };
-      building.stop();
-      process.stdout.write(stats.toString({
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false,
-      }) + '\n\n');
-      console.log(chalk.green('  webpack Build complete.\n'));
+        // copy(`src/${item}/*.js`, distPath, function (err, files) {
+        //   if (err) {
+        //     reject(err);
+        //     return;
+        //   };
+        //   console.log(chalk.green(`  copying ${item} complete.\n`));
+        //   if (index === folderList.length - 1) {
+        //     console.log(chalk.green('  Copy complete.\n'));
+        //     copying.stop();
+        //     resolve();
+        //   }
+        // });
+        // function replacePromise(filePath) {
+        //   return new Promise((resolve, reject) => {
+        //     const data = fs.readFileSync(filePath, { encoding: 'utf8' });
+        //     const result = data.replace(your - regex - or - text, replacement - text);
+        //     fs.writeFileSync(filePath, result, { encoding: 'utf8' });
+        //     resolve();
+        //   });
+        // }
+      });
       resolve();
     });
+  })
+  .then(() => {
+    console.log(chalk.cyan('## write package.json\n'));
+    fs.writeFileSync(path.resolve(distPath, 'package.json'), JSON.stringify(newPackage), {
+      flag: 'w',
+      encoding: 'utf8',
+    });
+    console.log(chalk.green('   write package.json complete.\n'));
+    return Promise.resolve();
+  })
+  .then(() => {
+    console.log(chalk.green('Build finish.\n'));
+  })
+  .catch((err) => {
+    throw err;
   });
-}).then(() => {
-  console.log(chalk.cyan('5.write package.json\n'));
-  fs.writeFileSync(path.resolve(distPath, 'package.json'), JSON.stringify(newPackage), {
-    flag: 'w',
-    encoding: 'utf8',
-  });
-  console.log(chalk.green('   write package.json complete.\n'));
-  return Promise.resolve();
-}).then(() => {
-  console.log(chalk.green('Build finish.\n'));
-}).catch((err) => {
-  throw err;
-});
 // new Promise((resolve, reject) => {
 //   // 构建全量压缩包
 //   const building = ora('building...');
